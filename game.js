d746432a410e068851790ceb2c75c350ccb92b18
Robert Gerboth <robert.gerboth@gmail.com>
2018-01-19 08:48:36 -0700

    A few housekeeping changes to code formatting, no functional changes

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // Psychic game js
 
-var wins = 0;
+    var wins = 0;
     var losses = 0;
     var guessesLeft = 9;
     var guessesSoFar = [];
@@ -10,33 +10,30 @@ var wins = 0;
     console.log(losses);
     console.log(guessesLeft);
 
-    // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+    // Creates an array that lists out all of the options 
     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     
-    // randommly chooses a choice from the options array. This is the Computer's guess.
+    // randommly chooses a choice from the options array. This is the Computer's first random letter.
     var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
-      // Determines which key was pressed.
-      var myChoice = event.key;
-      var myChoiceLower = myChoice.toLowerCase();
+      // Determines which key was pressed, forced to lower case.
+      var myChoiceLower = event.key.toLowerCase();
+
+      // Diagnostic code, facilitates testing
       console.log("The Die is Cast...")
       console.log("Random Letter: " + randomLetter);
       console.log("My Choice:     " + myChoiceLower);
-      // Alerts the key the user pressed (userGuess).
-      // alert("User guess: " + userGuess);
 
-      // Alerts the Computer's guess.
-      // alert("Computer guess: " + computerGuess);
+      // game logic
       if ((alphabet.indexOf(myChoiceLower)) >= 0) {  
         if (myChoiceLower === randomLetter) {
             wins ++;
             guessesLeft = 9;
             guessesSoFar = [];
             randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-            // alert("Winner. The Force is strong with this one...")
             document.getElementById("message").innerHTML = "Winner. You think you're awfully clever, don't you...";
 
         } else if (guessesLeft === 1) {
@@ -45,14 +42,12 @@ var wins = 0;
             guessesLeft = 9;
             guessesSoFar = [];
             randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-            // alert("Game Over, try again if you dare.");
             document.getElementById("message").innerHTML = "Game Over, try again if you dare.";
 
         } else {
             console.log(guessesLeft);
             guessesLeft --;
-            guessesSoFar.push(myChoice);
-            // alert("Concentrate and try again.");
+            guessesSoFar.push(myChoiceLower);
             document.getElementById("message").innerHTML = "Concentrate and try again.";
 
         }
